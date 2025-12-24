d291c16a94043a9bfd0715de2f72ea70db90444f
joooa <wangxinbei@sjtu.edu.cn>
2025-12-24 21:53:45 +0800

    feat account.h

---
+++ include/Account.h
@@ -0,0 +1,80 @@
+#ifndef BOOKSTORE_2025_ACCOUNT_H
+#define BOOKSTORE_2025_ACCOUNT_H
+#include "Storage.h"
+#include "MemoryRiver.h"
+struct Account {
+    char UserID[31];  // 数字，字母，下划线
+    char Password[31];  // 数字，字母，下划线
+    char Username[31];  // 除不可见字符以外 ASCII 字符
+    int Privilege;
+
+    Account(): Privilege(0) {
+        std::memset(UserID, 0, sizeof(UserID));
+        std::memset(Password, 0, sizeof(Password));
+        std::memset(Username, 0, sizeof(Username));
+    }
+
+    bool operator<(const Account& other) const {
+        return strcmp(UserID, other.UserID) < 0;
+    }
+};
+
+class AccountSystem{
+private:
+    BlockList<31, int> accountIndex; // 用户信息存储:UserID->accountStorage里的位置
+    MemoryRiver<Account> accountStorage;  // 账户数据存储
+
+    // 登录栈
+    struct LoginRecord {
+        char UserID[31];
+        int privilege;
+        char selected_ISBN[21];
+    };
+    std::vector<LoginRecord> loginStack;
+
+    bool init_root();
+    bool ID_check(const char* ID) const;
+    bool pwd_check(const char* pwd) const;
+    bool name_check(const char* name) const;
+    bool priv_check(int priv) const;
+public:
+    AccountSystem();
+    ~AccountSystem();
+
+    // 获取当前登录用户信息
+    char* get_curID() const;
+    int get_curpriv() const;
+    char* get_selected_ISBN() const;
+    void set_selected_ISBN(const char* ISBN);
+
+    // 登录：若成功则修改登录栈
+    // {0}
+    void su(const string& UserID, const string& Password = "");
+
+    // 撤销最后一次成功执行的 su 指令效果
+    // 若无已登录帐户则操作失败
+    // {1}
+    void logout();
+
+    // 注册权限等级为 {1} 的新账户
+    // 若userID与已注册用户重复则失败
+    // {0}
+    void regis(const string& UserID, const string& Password, const string& Username);
+
+    // 修改指定帐户的密码
+    // 如果该帐户不存在则操作失败；如果密码错误则操作失败
+    // 如果当前帐户权限等级为 {7} 则可以省略 [CurrentPassword]
+    // {1}
+    void passwd(const string& UserID, const string& NewPassword, const string& CurrentPassword = "");
+
+    // 创建账户
+    // 如果待创建帐户的权限等级>=当前帐户权限等级则操作失败；如果 [UserID] 与已注册帐户重复则操作失败。
+    // {3}
+    void useradd(const string& UserID, const string& Password, int Privilege, const string& Username);
+
+    // 删除账户
+    // 如果待删除帐户不存在/已登录则操作失败
+    // {7}
+    void deleteAccount(const string& UserID);
+};
+#endif //BOOKSTORE_2025_ACCOUNT_H
